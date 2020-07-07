657f84baa27a614138ba15754fd783fdd6524cf7
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-07-07 21:01:07 +0900

    update
    update

--- app/src/main/java/com/dontsu/countriesmvvmex/view/MainActivity.kt
+++ app/src/main/java/com/dontsu/countriesmvvmex/view/MainActivity.kt
@@ -2,11 +2,51 @@ package com.dontsu.countriesmvvmex.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dontsu.countriesmvvmex.R
+import com.dontsu.countriesmvvmex.viewmodel.RecyclerViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: RecyclerViewModel
+    private val countriesAdapter = CountryListAdapter(arrayListOf())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(RecyclerViewModel::class.java)
+        viewModel.refresh()
+
+        countriesList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = countriesAdapter
+        }
+
+        observeViewModel() //UI 업데이트를 담당할거임
+    }
+
+    private fun observeViewModel() {
+        viewModel.countries.observe(this, Observer { countries ->
+            countries?.let { countriesAdapter.updateCountries(it) }
+        })
+
+        viewModel.countryLoadError.observe(this, Observer { isError ->
+            isError?.let { list_error.visibility = if (it) View.VISIBLE else View.GONE }
+        })
+
+        viewModel.loading.observe(this, Observer { isLoading ->
+            isLoading?.let {
+                loading_view.visibility = if (it) View.VISIBLE else View.GONE
+                if (it) {
+                    list_error.visibility = View.GONE
+                    countriesList.visibility = View.GONE
+                }
+            }
+        })
     }
 }
